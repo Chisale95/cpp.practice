8b5c3baa20099c12423c414a7f609e3a5f0f15f0
Chisale Lekope <txotxoka95dede@gmail.com>
2026-03-26 04:04:56 +0200

    Add first C++ program: Hello World

---
+++ hello (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Simple function with clear purpose
+void greet() {
+    cout << "Hello, Github!" << endl;
+}
+
+int main() {
+    greet(); // call the function
+    return 0;
+}
